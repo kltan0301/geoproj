65ca8d74976191b08eedc8e3a4d6e85b1c12a75d
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-08-04 17:56:09 +0800

    Update user rating edit fix

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,15 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :attraction
+  after_initialize :init
 
   default_scope -> { order(created_at: :desc) }
 
   validates :title, presence: true
   validates :rating, :numericality => {:greater_than =>0 }, unless: :comments
   validates :comments, presence: true, unless: :rating
+
+  def init
+    self.rating ||= 0
+  end
 end
